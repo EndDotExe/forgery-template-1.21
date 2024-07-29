6f0abe124f8233bd07c978326e52233b64ded7ae
T Krishnan <thribhu.krishnan44@gmail.com>
2024-07-29 21:51:28 +1000

    Fixed the error of "Identifier has private access".

--- src/main/java/net/end/forgery/item/ModItems.java
+++ src/main/java/net/end/forgery/item/ModItems.java
@@ -7,18 +7,26 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import org.intellij.lang.annotations.Identifier;
+import net.minecraft.util.Identifier; //Use the correct package
 
 public class ModItems {
     public static final Item SMITHING_TEMPLATE = registerItem(new Item(new Item.Settings()));
 
 
-private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
-    entries.add(SMITHING_TEMPLATE);
-}
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SMITHING_TEMPLATE);
+    }
 
     private static Item registerItem(Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(Forgery.MOD_ID, "smithing_template"));
+        /*
+        * In the comments of the tutorial, you need to use Identifier.of (in the minecraft identifier package)
+        * instead of new Identifier
+        *
+        * Comment in question:
+        * "If anyone is having an error at 2:56 where it says "Identifier has private access",
+        * if you're modding for 1.21+ you need to replace 'new Identifier' with 'Identifier.of', and keep all the parameters the same."
+        */
+        return Registry.register(Registries.ITEM, Identifier.of(Forgery.MOD_ID, "smithing_template"), item);
     }
 
     public static void registerModItems() {
