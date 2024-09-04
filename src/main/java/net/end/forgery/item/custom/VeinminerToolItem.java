10bf6531e2137a06ffd6906a5aca049804668bd5
EndDotExe <hughiemck@icloud.com>
2024-09-04 10:07:14 +1000

    :3

--- src/main/java/net/end/forgery/item/custom/VeinminerToolItem.java
+++ src/main/java/net/end/forgery/item/custom/VeinminerToolItem.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
@@ -14,9 +15,18 @@ import java.util.Map;
 public class VeinminerToolItem extends Item {
     private static final Map<Block, Block> VEINMINERTOOL_MAP =
             Map.of(
-                    Blocks.COAL_ORE, Blocks.AIR
+                    Blocks.COAL_ORE, Blocks.AIR,
+                    Blocks.DIAMOND_ORE, Blocks.AIR,
+                    Blocks.IRON_ORE, Blocks.AIR,
+                    Blocks.COPPER_ORE, Blocks.AIR,
+                    Blocks.GOLD_ORE, Blocks.AIR,
+                    Blocks.LAPIS_ORE, Blocks.AIR,
+                    Blocks.EMERALD_ORE, Blocks.AIR,
+                    Blocks.DEEPSLATE_COAL_ORE, Blocks.AIR,
+                    Blocks.DEEPSLATE_DIAMOND_ORE, Blocks.AIR,
+                    Blocks.DEEPSLATE_IRON_ORE, Blocks.AIR
 
-                    );
+            );
 
     public VeinminerToolItem(Settings settings) {
         super(settings);
