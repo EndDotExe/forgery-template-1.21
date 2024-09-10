66b51e1d24b5091a225ab8f3eadc498d22da6a51
EndDotExe <hughiemck@icloud.com>
2024-09-11 08:58:47 +1000

    :3c

---
+++ src/main/java/net/end/forgery/item/custom/BlockInteractionRangeTool.java
@@ -0,0 +1,16 @@
+package net.end.forgery.item.custom;
+
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.ActionResult;
+
+public class BlockInteractionRangeTool extends ToolItem {
+
+
+    public BlockInteractionRangeTool(ToolMaterial material, Settings settings) {
+        super(material, settings);
+    }
+
+
+}
