d9dbc688a7c4033fe4a6aadb970799895b75184d
EndDotExe <hughiemck@icloud.com>
2024-10-18 12:20:14 +1100

    Adding a new material for upgraded tools
    :3c

---
+++ src/main/java/net/end/forgery/item/ModToolMaterials.java
@@ -0,0 +1,60 @@
+package net.end.forgery.item;
+
+import com.google.common.base.Suppliers;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    FORGERY_UPGRADED (BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 10000, 10.0F, 5.0F, 0, () -> {
+        return Ingredient.ofItems(new ItemConvertible[]{Items.NETHERITE_INGOT});
+    });
+
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private ModToolMaterials(final TagKey inverseTag, final int itemDurability, final float miningSpeed, final float attackDamage, final int enchantability, final Supplier repairIngredient) {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        Objects.requireNonNull(repairIngredient);
+        this.repairIngredient = (Supplier<Ingredient>) Suppliers.memoize(repairIngredient::get);
+    }
+
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public TagKey<Block> getInverseTag() {
+        return this.inverseTag;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+}
---
+++ src/main/java/net/end/forgery/util/ModTags.java
@@ -0,0 +1,7 @@
+package net.end.forgery.util;
+
+import net.minecraft.registry.tag.TagKey;
+
+public class ModTags {
+
+}
