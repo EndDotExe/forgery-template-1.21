30e9225185e78e56a36f351131564fcdd9801c5e
EndDotExe <hughiemck@icloud.com>
2024-10-25 09:09:08 +1100

    I FINALLY FIXED THE MIXINS ISSUE (i hope)

--- src/main/java/net/end/forgery/mixin/ExampleMixin.java
+++ src/main/java/net/end/forgery/mixin/ExampleMixin.java
@@ -1,6 +1,8 @@
 package net.end.forgery.mixin;
 
+import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.util.math.BlockPos;
@@ -10,10 +12,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SimpleInventory.class)
+@Mixin(ReachEntityAttributes.class)
 public class ExampleMixin {
-	@Inject(method = "method_49106", at = @At("HEAD"), cancellable = true)
-	private void checkWithinActualReach(BlockEntity blockEntity, PlayerEntity playerEntity, int i, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method = "getAttackRange", at = @At("HEAD"), cancellable = false)
+	private static void checkWithinActualReach(LivingEntity entity, double baseAttackRange, CallbackInfoReturnable<Double> cir) {
 		// Your custom logic here
 	}
 }
